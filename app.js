2a09358c5d98afad3b7ac1398c2bdfb1f016c8dc
Kundan Prasad <kp800532@gmail.com>
2022-11-03 00:10:47 +0530

    Added Hashing Function

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
-console.log(process.env.API_KEY);
+console.log(md5("123456"));
 
 
 app.set('view engine','ejs');
@@ -22,7 +22,6 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User",userSchema);
 
@@ -42,7 +41,7 @@ app.post("/register",function(req,res){
 
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save(function(err){
@@ -56,14 +55,16 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     
     User.findOne({email: username},function(err,foundUser){
         if(err){
             console.log(err);
         }else{
             if(foundUser){
+                if(foundUser.password === password){
                 res.render("secrets");
+                }
             }
         }
     });
