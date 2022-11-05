376c1afd9e1e5d7ad3be978e786eec2ccd6072a8
Kundan Prasad <kp800532@gmail.com>
2022-11-05 18:00:20 +0530

    Google OAuth 2.0 Authentication

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 
@@ -27,20 +29,53 @@ mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User",userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user.id); 
+   // where is this user.id going? Are we supposed to access this anywhere?
+});
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
 
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+   
 app.get("/",function(req,res){
     res.render("home");
+});
+
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ["profile"]
+}));
+
+app.get("/auth/google/secrets", 
+  passport.authenticate("google", { failureRedirect: "/login"}),
+  function(req, res) {
+    // Successful authentication, redirect secrets.
+    res.redirect('/secrets');
 });
 
 app.get("/login",function(req,res){
@@ -74,7 +109,7 @@ app.post("/register",function(req,res){
     User.register({username: req.body.username}, req.body.password,function(err,user){
         if(err){
             console.log(err);
-            res.redirect("/regoster");
+            res.redirect("/register");
         }else{
             passport.authenticate("local")(req,res,function(){
                 res.redirect("/secrets");
